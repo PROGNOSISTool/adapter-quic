e4303db3a66084cbf61a361d9989d0478062abee
Tiago Ferreira <me@tiferrei.com>
2020-09-17 19:48:59 +0100

    feat: clean up old OT

--- adapter/json-cli.go
+++ adapter/json-cli.go
@@ -6,49 +6,58 @@ import (
 	"syscall"
 )
 
-const NODE_PROGRAM string = `const fs=require('fs'),readFiles=()=>{const e=fs.readdirSync('.').filter(e=>e.match(/oracleTable(-\d+)?\.json/g));console.log('Combining Oracle Tables: '+e);let s=[];for(const c of e)try{const e=fs.readFileSync(c),o=JSON.parse(e);s.push(o)}catch(e){console.log(e),process.exit(1)}return s},combineObjects=e=>Object.assign({},...e),saveObject=(e,s)=>{const c=JSON.stringify(e);try{fs.writeFileSync(s,c)}catch(e){console.log(e),process.exit(1)}},main=()=>{const e=readFiles();oracleTable=combineObjects(e),saveObject(oracleTable,'oracleTable.json')};main();`
-//`#!/usr/bin/env node
+const NODE_PROGRAM string = `const fs=require('fs'),selectFiles=()=>fs.readdirSync('.').filter(e=>e.match(/oracleTable(-\d+)?\.json/g)),readFiles=e=>{console.log('Combining Oracle Tables: '+e);let s=[];for(const c of e)try{const e=fs.readFileSync(c),o=JSON.parse(e);s.push(o)}catch(e){console.log(e),process.exit(1)}return s},removeFiles=e=>{for(const s of e)fs.unlinkSync(s)},combineObjects=e=>Object.assign({},...e),saveObject=(e,s)=>{const c=JSON.stringify(e);try{fs.writeFileSync(s,c)}catch(e){console.log(e),process.exit(1)}},main=()=>{const e=selectFiles(),s=readFiles(e),c=combineObjects(s);saveObject(c,'oracleTable.json');const o=e.filter(e=>'oracleTable.json'!==e);removeFiles(o)};main();`
+//#!/usr/bin/env node
 //const fs = require('fs');
 //
-//const readFiles = () => {
-//	const filenames = fs.readdirSync('.')
-//		.filter((filename) => filename.match(/oracleTable(-\d+)?\.json/g))
-//	console.log('Combining Oracle Tables: ' + filenames)
-//	let objects = []
-//	for (const file of filenames) {
-//		try {
-//			const jsonString = fs.readFileSync(file)
-//			const oracleTable = JSON.parse(jsonString)
-//			objects.push(oracleTable)
-//		} catch(err) {
-//			console.log(err)
-//			process.exit(1);
-//		}
-//	}
-//	return objects
+//const selectFiles = () => fs.readdirSync('.')
+//.filter((filename) => filename.match(/oracleTable(-\d+)?\.json/g))
+//
+//const readFiles = (filenames) => {
+//console.log('Combining Oracle Tables: ' + filenames)
+//let objects = []
+//for (const file of filenames) {
+//try {
+//const jsonString = fs.readFileSync(file)
+//const oracleTable = JSON.parse(jsonString)
+//objects.push(oracleTable)
+//} catch(err) {
+//console.log(err)
+//process.exit(1);
+//}
+//}
+//return objects
+//}
+//
+//const removeFiles = (filenames) => {
+//for (const file of filenames) {
+//fs.unlinkSync(file)
+//}
 //}
 //
 //const combineObjects = (objects) => Object.assign({}, ...objects)
 //
 //const saveObject = (object, filename) => {
-//	const jsonString = JSON.stringify(object)
-//	try {
-//		fs.writeFileSync(filename, jsonString)
-//	} catch(err) {
-//		console.log(err)
-//        process.exit(1);
-//	}
+//const jsonString = JSON.stringify(object)
+//try {
+//fs.writeFileSync(filename, jsonString)
+//} catch(err) {
+//console.log(err)
+//process.exit(1);
+//}
 //
 //}
 //
 //const main = () => {
-//	const oracleTables = readFiles()
-//	oracleTable = combineObjects(oracleTables)
-//	saveObject(oracleTable, 'oracleTable.json')
+//const filesToMerge = selectFiles()
+//const oracleTables = readFiles(filesToMerge)
+//const oracleTable = combineObjects(oracleTables)
+//saveObject(oracleTable, 'oracleTable.json')
+//const filesToDelete = filesToMerge.filter((filename) => filename !== "oracleTable.json")
+//removeFiles(filesToDelete)
 //}
 //
 //main()
-//`
 
 func hasNodeInstalled() (bool, error) {
     _, err := exec.Command("sh","-c","command -v node").Output()
