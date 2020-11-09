e4d281b9a9489cb464f711ca224c97a588f0669b
Tiago Ferreira <me@tiferrei.com>
2020-11-09 15:04:50 +0000

    fix: max data ceiling

--- agents/flow_control_agent.go
+++ agents/flow_control_agent.go
@@ -292,12 +292,13 @@ func (a *FlowControlAgent) Run(conn *Connection) { // TODO: Report violation of 
 				} else {
 					a.Logger.Printf("INFO: Flow Control Queue empty, sending new %v frame at %v enc level", fr.FrameType.String(), fr.EncryptionLevel.String())
 					conn.TLSTPHandler.MaxStreamDataBidiLocal = 160
-					
+					conn.TLSTPHandler.MaxData = 64 * 1024
+
 					switch fr.FrameType {
 					case MaxDataType:
 						conn.FrameQueue.Submit(QueuedFrame{&MaxDataFrame{conn.TLSTPHandler.MaxData}, fr.EncryptionLevel})
 					case MaxStreamDataType:
-						conn.FrameQueue.Submit(QueuedFrame{&MaxStreamDataFrame{a.conn.CurrentStreamID - 4, conn.TLSTPHandler.MaxStreamDataBidiLocal}, fr.EncryptionLevel})
+						conn.FrameQueue.Submit(QueuedFrame{&MaxStreamDataFrame{uint64(Max(int(a.conn.CurrentStreamID - 4), 0)), conn.TLSTPHandler.MaxStreamDataBidiLocal}, fr.EncryptionLevel})
 					}
 				}
 			case <-a.close:
